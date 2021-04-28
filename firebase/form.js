527cea3323336fa25786dfa9988ee0a17cccd5f2
IamDuke <dukekim47@gmail.com>
2021-04-28 18:25:06 +0900

    Working Sign Up with email and password
    Firebase Auth installed

--- firebase/form.js
+++ firebase/form.js
@@ -3,8 +3,6 @@
     apiKey: "AIzaSyCwwqK3_ZSTidDRwNEE-T50j9PKQ6wX0Ws",
     authDomain: "personal-library-c5a4b.firebaseapp.com",
     projectId: "personal-library-c5a4b",
-    storageBucket: "personal-library-c5a4b.appspot.com",
-    messagingSenderId: "396802370606",
     appId: "1:396802370606:web:43cc8dd374e4a8c4a083bf"
   };
   // Initialize Firebase
@@ -12,11 +10,10 @@
 
   const auth = firebase.auth();
 
-
   function signUp() {
       let email = document.getElementById("email-input-su");
       let password = document.getElementById("password-input-su");
-      auth.createUserWithEmailandPassword(email.value, password.value)
+      auth.createUserWithEmailAndPassword(email.value, password.value)
       .then((userCredential) => {
           let user = userCredential.user;
       })
@@ -25,3 +22,4 @@
           let errorMessage = error.message;
       });
   }
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//Firebase Sign Up - Email and Passowrd//
 const signUpBtn = document.getElementById("sign-up-user");
-
 signUpBtn.addEventListener("click", signUp)
+

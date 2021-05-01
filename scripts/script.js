fe94afd3d0b7eadb855ed3f0d7e2796cdc7d0a3b
IamDuke <dukekim47@gmail.com>
2021-05-01 23:41:09 +0900

    renamed functions
    renamed close modal functions

--- scripts/script.js
+++ scripts/script.js
@@ -26,25 +26,31 @@ form.addEventListener("submit", (e) => {
 })
 
 const body = document.getElementsByTagName("body");
+const openModal = document.getElementById("openModal");
 const editModalForm = document.getElementById("edit-card");
+const editModal = document.getElementById("edit-container");
 
 
 
 //Brings Data from Firestore into DOM into main content container. Assigns Unique Id and content of each document into HTML
 const renderCard = (changes) => {
-    let html = "";
     changes.forEach(book => {
         let uniqueID = book.doc.id;
         let htmlID = `_${book.doc.id}`;
         const cards = book.doc.data();
+
+        //Below Creates A Bootstrap Modal to Main Content
         const outerDiv = document.createElement("div");
         outerDiv.setAttribute("data-toggle", "modal");
         outerDiv.setAttribute("data-target", "#openModal");
         outerDiv.classList.add("card");
-        outerDiv.setAttribute("data-id", htmlID)
+        outerDiv.setAttribute("data-id", htmlID);
+
+        //Event Listener to Retrieve Data from Selected Card for Editting 
         outerDiv.addEventListener("click", (e) => {
             let thisCard = e.target.parentNode.parentNode;
             let thisID = thisCard.getAttribute("data-id").slice(1);
+            editModal.setAttribute("data-id", thisID);
             db.collection("library").doc(thisID).get().then(snapshot => {
                 let retrievedData = snapshot.data();
                 editModalForm.title.value = retrievedData.title;
@@ -53,6 +59,8 @@ const renderCard = (changes) => {
                 editModalForm.checkbox.checked = retrievedData.read;
             });
         });
+        //Event Listener to Retrieve Data from Selected Card for Editting (End)
+
         const innerDiv = document.createElement("div");
         innerDiv.classList.add("card-body");
         const heading = document.createElement("h5");
@@ -62,10 +70,14 @@ const renderCard = (changes) => {
         button.setAttribute("type", "button");
         button.classList.add("close-btn", "close-card");
         button.innerHTML = "&times;";
+
+        //Event Listener to Delete Data from Firestore
         button.addEventListener("click", (e) => {
             e.stopPropagation();
             db.collection("library").doc(uniqueID).delete();
         })
+        ////Event Listener to Delete Data from Firestore (End)
+
         const text = document.createElement("p");
         text.classList.add("card-text");
         text.textContent = cards.content;
@@ -74,9 +86,9 @@ const renderCard = (changes) => {
         footer.textContent = cards.footer;
         const readStatus = document.createElement("p");
 
-
+        //Realtime Database Interaction
+        //Added Situation / On load of page
         if (book.type === "added") {
-            console.log(book, book.type)
             mainContent.appendChild(outerDiv);
             outerDiv.appendChild(innerDiv);
             innerDiv.appendChild(heading);
@@ -92,9 +104,24 @@ const renderCard = (changes) => {
                 readStatus.classList.add("not-read-card");
                 footer.appendChild(readStatus);
             }
+            // Removed Situation where data is manually removed by user
         } else if (book.type === "removed") {
             let removedCard = document.querySelector(`[data-id = _${uniqueID}]`);
             mainContent.removeChild(removedCard);
+            // Modified Situation where data is mutated by user
+        } else if (book.type === "modified") {
+            let updatedCard = document.querySelector(`[data-id = _${uniqueID}]`);
+            console.log(updatedCard);
+            updatedCard.childNodes[0].childNodes[0].textContent = cards.title;
+            updatedCard.childNodes[0].childNodes[2].textContent = cards.content;
+            updatedCard.childNodes[1].childNodes[0].textContent = cards.footer;
+            if (cards.read) {
+                updatedCard.childNodes[1].childNodes[1].textContent = "Complete";
+                updatedCard.childNodes[1].childNodes[1].className = "read-card";
+            } else {
+                updatedCard.childNodes[1].childNodes[1].textContent = "Incomplete";
+                updatedCard.childNodes[1].childNodes[1].className = "not-read-card";
+            }
         }
     }
     );
@@ -102,34 +129,34 @@ const renderCard = (changes) => {
 
 // Updating Data within Cards
 
+const edit = document.getElementById("edit-card");
+edit.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let uniqueID = edit.parentElement.getAttribute("data-id");
+    db.collection("library").doc(uniqueID).update({
+        title: edit.title.value,
+        content: edit.content.value,
+        footer: edit.footer.value,
+        read: edit.checkbox.checked
+    });
+    closeModal("openModal");  
+})
 
-function openCard(outerDiv) {
-    outerDiv.addEventListener("click", () => {
 
 
-    })
+function closeModal(modalId) {
+    // Retrieves the modal
+    const modal = document.getElementById(modalId);
 
-}
+    // Alters bootsrap hidden modal
+    modal.classList.remove('show');
+    modal.setAttribute('aria-hidden', 'true');
+    modal.setAttribute('style', 'display: none');
 
-function editModalCard(cards) {
-    let editCard = document.getElementById("edit-card");
-    editCard.title.value = cards.title
-    editCard.content.value = cards.content
-    editCard.footer.value = cards.footer
+     // Retrieves Backdrop from Bootstrap
+     const modalBackdrops = document.getElementsByClassName('modal-backdrop');
 
-    let checkbox = document.getElementById("update-read");
-    if (cards.read) {
-        let checkbox = document.getElementById("update-read");
-        checkbox.checked = true;
-    } else { checkbox.checked = false }
-}
+     // remove opened modal backdrop
+      document.body.removeChild(modalBackdrops[0]);
+  }
 
-function removeEditModal() {
-    let openModal = document.getElementById("openModal");
-    body.removeChild = openModal;
-}
-
-function editCard(e) {
-    let card = e.target.querySelector("div[data-id]");
-    console.log(card)
-}
